fac75bc73ca5ebd7f262d3e4aa4e244f299f9f8b
Ricardo Faria <r1c4rd0f4@gmail.com>
2016-04-04 23:22:11 +0100

    add input D02 and calculate D03 & D02 median with correction, add output plots for coords stations, the minor updates

--- ReSun_analysis.R
+++ ReSun_analysis.R
@@ -2,6 +2,8 @@
 
 #packages:
 #library(xlsx)
+#library(plyr)
+library(reshape2)
 library(lubridate)
 library(timeDate)
 library(ncdf4)
@@ -44,7 +46,7 @@ source("matrix_rotation.R")
 
 
 #create folders
-system("mkdir -p output/kml output/images output/nc")
+system("mkdir -p output/kml output/images output/nc output/graphs")
 
 #cores dos graficos
 rgb.palette.rad <- colorRampPalette(c("lightcyan", "yellow2", "orange", "tomato1", "violetred4", "violetred", "purple"), space = "rgb")
@@ -91,7 +93,6 @@ for (l in 1:nrow(locs)) {
       
 }
 
-
 hgt <- ncvar_get(nc)[,,3]
 
 ncols <- length(long)
@@ -135,19 +136,29 @@ if (corr == 1) {
             
             #corr_IGPH[[i]] <- ncvar_get(nc)[,,8]
             
-            m <- corr_values[c("m_D03"),c(corr_vars[i])]
-            x <- corr_values[c("x_D03"),c(corr_vars[i])]
+            # dominio D02
+            m <- corr_values[c("m_D02"), c(corr_vars[i])]
+            x <- corr_values[c("x_D02"), c(corr_vars[i])]
             
-            corr_fix[[fix_var[i]]] <- x + m*get(fix_var_tmp[i])
+            corr_fix[[paste0(fix_var[i], "_D02")]] <- x + m*get(fix_var_tmp[i])
+            
+            # dominio D03
+            m <- corr_values[c("m_D03"), c(corr_vars[i])]
+            x <- corr_values[c("x_D03"), c(corr_vars[i])]
+            
+            corr_fix[[paste0(fix_var[i], "_D03")]] <- x + m*get(fix_var_tmp[i])
             
             if (corr_vars[i] == "lat") {
                   
-                  corr_fix[[fix_var[i]]] <- matrix(data = corr_fix$lat, nrow = nrow(hgt), ncol = ncol(hgt), byrow = F)
+                  corr_fix[[paste0(fix_var[i], "_D02")]] <- matrix(data = corr_fix$lat, nrow = nrow(hgt), ncol = ncol(hgt), byrow = F)
+                  corr_fix[[paste0(fix_var[i], "_D03")]] <- matrix(data = corr_fix$lat, nrow = nrow(hgt), ncol = ncol(hgt), byrow = F)
                   
             } else if (corr_vars[i] == "lon") {
                   
-                  corr_fix[[fix_var[i]]] <- matrix(data = corr_fix$lon, nrow = nrow(hgt), ncol = ncol(hgt), byrow = T)
-            }
+                  corr_fix[[paste0(fix_var[i], "_D02")]] <- matrix(data = corr_fix$lon, nrow = nrow(hgt), ncol = ncol(hgt), byrow = T)
+                  corr_fix[[paste0(fix_var[i], "_D03")]] <- matrix(data = corr_fix$lon, nrow = nrow(hgt), ncol = ncol(hgt), byrow = T)
+                  
+                  }
       }
 }
 
@@ -169,6 +180,7 @@ DIFGPH <- c()
 Kt <- c()
 fix_var <- intersect(corr_vars, c("Kt", "cleS", "iS", "cloS"))
 d <- c()
+d_tot <- c()
 months_name <- c("tot", seq_months)
 # ciclo abrir ficheiros e aplicar correcao variavel kt - varia em funcao do tempo
 # retirar valores das variaveis
@@ -178,22 +190,42 @@ for (i in 1:length(fileNames)){
       
       # retirar valores das variaveis
       IGPH[[i]] <- ncvar_get(temp_nc)[,,8] 
+      # crop ao contour hgt/land
+      #IGPH[[1]][hgt <= 0.1] <- NA
+      
       Kt[[i]] <-ncvar_get(temp_nc)[,,4]
       IDIR[[i]] <- ncvar_get(temp_nc)[,,7]
       IDIF[[i]] <- ncvar_get(temp_nc)[,,6]  
       DIFGPH[[i]] <- ncvar_get(temp_nc)[,,5]  
-      
+
       if (i == 1 & corr == 1 & length(fix_var) > 0) {
             
             for (j in 1:length(fix_var)) {
                   
-                  m <- corr_values[c("m_D03"),c(fix_var[j])]
-                  x <- corr_values[c("x_D03"),c(fix_var[j])]
-                  
-                  corr_fix[[paste0(fix_var[j],i)]] <- x + m*Kt[[i]]
-                  
-                  IGPH_corr[[i]] <- IGPH[[i]] * (1 - (corr_fix[[paste0(fix_var[j],i)]] + corr_fix_fix)/100)
-                  #IGPH_corr[[i]] <- IGPH[[i]] + corr_fix[[paste0(fix_var[j],i)]] + corr_fix_fix
+                  # i >= 1 & i <= 13 equivalente ao dominio D02
+                  if (i == 1) {
+                        
+                        m <- corr_values[c("m_D02"),c(fix_var[j])]
+                        x <- corr_values[c("x_D02"),c(fix_var[j])]
+                        
+                        corr_fix[[paste0(fix_var[j], "_D02")]] <- x + m*Kt[[i]]
+                        
+                        IGPH_corr[[1]] <- IGPH[[1]] * (1 - (corr_fix[[paste0(fix_var[j], "_D02")]] + corr_fix_fix)/100)
+                        #IGPH_corr[[i]] <- IGPH[[i]] + corr_fix[[paste0(fix_var[j],i)]] + corr_fix_fix
+                        
+                  } 
+                  # i > 13 & i <= 26 equivalente ao dominio D03
+                  else if (i == 14 ) {
+                       
+                        m <- corr_values[c("m_D03"),c(fix_var[j])]
+                        x <- corr_values[c("x_D03"),c(fix_var[j])]
+                        
+                        corr_fix[[paste0(fix_var[j], "_D03")]] <- x + m*Kt[[i]]
+                        
+                        IGPH_corr[[2]] <- IGPH[[2]] * (1 - (corr_fix[[paste0(fix_var[j], "_D03")]] + corr_fix_fix)/100)
+                        #IGPH_corr[[i]] <- IGPH[[i]] + corr_fix[[paste0(fix_var[j],i)]] + corr_fix_fix
+                        
+                  }
                   
             }
             
@@ -201,24 +233,12 @@ for (i in 1:length(fileNames)){
             
             IGPH_corr[[i]] <- IGPH[[i]] * (1 - (corr_fix_fix)/100)
             
+      } else if (i == 14 & corr == 1 & length(fix_var) == 0) {
+            
+            IGPH_corr[[2]] <- IGPH[[i]] * (1 - (corr_fix_fix)/100)
+            
       }
       
-      for (l in 1:length(lat_index)) {
-            
-            
-            if (i == 1 & corr == 1) {    
-                  
-                  d <- rbind(d ,c(Location = levels(locs$loc)[l], value = IGPH_corr[[i]][lat_index[l],long_index[l]], Month = "tot_corr"))
-                  
-                  d <- rbind(d ,c(Location = levels(locs$loc)[l], value = IGPH[[1]][lat_index[l],long_index[l]], Month = months_name[1]))
-                  
-            } else if (i >= 2 & i <= length(fileNames)){ #if (i >= 2 & i <= length(fileNames))
-                  
-                  d <- rbind(d ,c(Location = levels(locs$loc)[l], value = IGPH[[i]][lat_index[l],long_index[l]], Month = months_name[i]))
-                  
-            }
-            
-      }
       nc_close(temp_nc)
       
       #retirar valor maximo de radiacao para grafico
@@ -235,12 +255,121 @@ for (i in 1:length(fileNames)){
       
 }
 
+#retirar valores das cooredenadas das estacoes de entrada
+for (i in 1:(length(fileNames)/2)){ 
+      for (l in 1:length(lat_index)) {
+            
+            if (i == 1 & corr == 1) { 
+                  
+                  #d_tot <- rbind(d_tot ,c(Location = levels(locs$loc)[l], value = IGPH_corr[[i]][lat_index[l], long_index[l]], Month = "TMY_corr"))
+                  
+                  d_tot <- rbind(d_tot ,c(Location = levels(locs$loc)[l], value = (IGPH_corr[[1]][lat_index[l], long_index[l]] + IGPH_corr[[2]][lat_index[l], long_index[l]])/2, Month = "TMY_corr"))
+                  
+            }
+            
+      }
+      for (l in 1:length(lat_index)) {
+            
+            if (i == 1) {    
+                  
+                  #d <- rbind(d ,c(Location = levels(locs$loc)[l], value = IGPH_corr[[i]][lat_index[l], long_index[l]], Month = "tot_corr"))
+                  #d_tot <- rbind(d_tot ,c(Location = levels(locs$loc)[l], value = IGPH[[1]][lat_index[l], long_index[l]], Month = months_name[1]))
+                  
+                  d_tot <- rbind(d_tot ,c(Location = levels(locs$loc)[l], value = (IGPH[[1]][lat_index[l],long_index[l]] + IGPH[[1 + 13]][lat_index[l], long_index[l]])/2, Month = months_name[1]))
+                  
+            } else if (i >= 2 & i <= length(fileNames)){ #if (i >= 2 & i <= length(fileNames))
+                  
+                  #d <- rbind(d ,c(Location = levels(locs$loc)[l], value = IGPH[[i]][lat_index[l], long_index[l]], Month = months_name[i]))
+                  
+                  d <- rbind(d ,c(Location = levels(locs$loc)[l], value = (IGPH[[i]][lat_index[l], long_index[l]] + IGPH[[i + length(fileNames)/2]][lat_index[l], long_index[l]])/2, Month = months_name[i]))
+                  
+            }
+            
+      }
+}
+
+#separar dominio D02 e D03 e faxer media dos 2 contando com os fatores de correcao
+for (i in 1:(length(fileNames)/2)) {
+      
+      if (i == 1 & corr == 1) {
+            
+            IGPH_corr[[1]] <- (IGPH_corr[[1]] + IGPH_corr[[2]])/2
+            
+      }
+      
+      IGPH[[i]] <- (IGPH[[i]] + IGPH[[i + length(fileNames)/2]])/2
+      Kt[[i]] <- (Kt[[i]] + Kt[[i + length(fileNames)/2]])/2
+      IDIR[[i]] <- (IDIR[[i]] + IDIR[[i + length(fileNames)/2]])/2
+      IDIF[[i]] <- (IDIF[[i]] + IDIF[[i + length(fileNames)/2]])/2
+      DIFGPH[[i]] <- (DIFGPH[[i]] + DIFGPH[[i + length(fileNames)/2]])/2
+
+}
+# remover resto das matrizes nao necessarias para libertar espaco
+for (i in 1:length(fileNames)/2) {
+      
+      if (i == 1 & corr == 1) {
+            
+            IGPH_corr[[2]] <- NULL
+            
+      }
+      
+      IGPH[[i + length(fileNames)/2]] <- NULL
+      Kt[[i + length(fileNames)/2]] <- NULL
+      IDIR[[i + length(fileNames)/2]] <- NULL
+      IDIF[[i + length(fileNames)/2]] <- NULL
+      DIFGPH[[i + length(fileNames)/2]] <- NULL
+      
+}
+
 # save d table for revisado results comparation
-write.csv(d, file = "output/IGPH_coords.csv", row.names = T)
+write.csv(rbind(d, d_tot), file = paste0("output/IGPH_", local, "_stat_data.csv"), row.names = T)
 #write.xlsx(d, file = "output/IGPH_coords.csv")
 
+d <- data.frame(d)
+d_tot <- data.frame(d_tot)
+d$value <- as.numeric(as.character(d$value))
+d_tot$value <- as.numeric(as.character(d_tot$value))
+d$Month <- factor(d$Month, levels = d$Month)
+d_tot$Month <- factor(d_tot$Month, levels = d_tot$Month)
+
+#d_d <- d[order(d[,1]),] 
+#d_d <- melt(d_d, id = c("value", "Month"))
+
+# qplot(x = d$Month, y = d$value)
+# qplot(x = d$Month, y = d$value, color = d$Location)
+
+g_poli <- ggplot(data = d, aes(x = Month ,y = value, color = Location, group = Location)) + 
+      geom_point(size = 2) + 
+      geom_line(alpha = .3, size=1) + 
+      scale_color_brewer(palette="Paired") + 
+      theme_bw() +
+      ggtitle("Radiação mensal nas EMAS [W/m^2]")
+ggsave(plot = g_poli, paste0("output/graphs/stations_", local, "_tot_year_pont-lin.png"), width = 7*7, height = 5*7, units = "cm", scale = .6, dpi = 400)
+
+g_hist <- ggplot(data = d, aes(x = Month ,y = value, fill = Location)) + 
+      geom_bar(stat = "identity", position = "dodge") + 
+      scale_fill_brewer(palette="Paired") + 
+      theme_bw() +
+      ggtitle("Radiação mensal nas EMAS [W/m^2]")
+ggsave(plot = g_hist, paste0("output/graphs/stations_", local, "_tot_year_hist.png"), width = 7*7, height = 5*7, units = "cm", scale = .6, dpi = 400)
+
+g_hist_tot <- ggplot(data = d_tot, aes(x = Month ,y = value, fill = Location)) + 
+      geom_bar(stat = "identity", position = "dodge") +
+      scale_fill_brewer(palette="Paired") + 
+      theme_bw() +
+      ggtitle("Radiação total e corrigida anual nas EMAS [W/m^2]")
+ggsave(plot = g_hist_tot, paste0("output/graphs/stations_", local, "_tot_year_hist_tot.png"), width = 7*7, height = 5*7, units = "cm", scale = .6, dpi = 400)
+
+g_polar <- ggplot(data = d, aes(x = Month ,y = value, fill = Location)) + 
+      geom_bar(stat = "identity", position = "dodge") + 
+      coord_polar() +
+      scale_fill_brewer(palette="Paired") + 
+      theme_bw() +
+      ggtitle("Radiação mensal nas EMAS [W/m^2]")
+ggsave(plot = g_polar, paste0("output/graphs/stations_", local, "_tot_year_polar-hist.png"), width = 6*7, height = 5*7, units = "cm", scale = .6, dpi = 400)
+
 # criar raster files cortados pelo contorno da topografia
-message("creating raster files")
+message("cropping & creating raster files")
 raster_IGPH <- c()
 raster_IGPH_corr <- c()
 raster_IDIR <- c()
@@ -386,6 +515,8 @@ names(raster_Kt) <- paste0("Kt_", months_name[1:length(fileNames)])
 #raster_DIFGPH <- rts(stack(raster_DIFGPH), seq_time[1:length(fileNames)])
 #raster_Kt <- rts(stack(raster_Kt), seq_time[1:length(fileNames)])
 
+save(locs, d, d_tot, raster_IGPH, rgb.palette.rad, file = "output/data.RData")
+
 #ciclo gerar mapas e kmz
 for (j in 1:length(variavs)) {
       
@@ -411,7 +542,7 @@ for (j in 1:length(variavs)) {
                         
                         max_axis <- ceiling(as.numeric(max(get(paste0("max_", variavs[j])))))
                         
-                        name_png = paste0("output/images/", variavs[j], "_", months_name[i], ".png")
+                        name_png = paste0("output/images/", variavs[j], "_", local, "_", months_name[i], ".png")
                         png(name_png, width = ncols*7, height = nrows*7, units = "px", res = 250)  #width = 7000 (width = 14000, height = 9000, units = "px", res = 1000)
                         
                         filled.contour(long, lat, t(get(variav_name)[,,i]),  color = color, levels = seq(0, max_axis, max_axis/15), # nlevels = 400, #axes = F #(12), nlev=13,
@@ -436,7 +567,7 @@ for (j in 1:length(variavs)) {
                   
                   max_axis <- ceiling(as.numeric(max(get(paste0("max_", variavs[j])))))
                   
-                  name_png = paste0("output/images/", variavs[j], "_", months_name[i], ".png")
+                  name_png = paste0("output/images/", variavs[j], "_", local, "_", months_name[i], ".png")
                   png(name_png, width = ncols*7, height = nrows*7, units = "px", res = 250)  #width = 7000 (width = 14000, height = 9000, units = "px", res = 1000)
                   
                   filled.contour(long, lat, t(get(variav_name)[,,i]),  color = color, levels = seq(0, max_axis, max_axis/15), # nlevels = 400, #axes = F #(12), nlev=13,
@@ -483,12 +614,12 @@ for (j in 1:length(variavs)) {
             # gravar sequencia dos meses
             if (j != 2) {
                   
-                  KML(get0(variav_name), file = paste0(variavs[j], ".kml"), time = c(tS_i, tS_i[length(tS_i)]), overwrite = T, #[length(tS_f)]
+                  KML(get0(variav_name), file = paste0(variavs[j], "_", local, ".kml"), time = c(tS_i, tS_i[length(tS_i)]), overwrite = T, #[length(tS_f)]
                       col = color(20), blur = 1) 
                   
             }else if (j == 2) {
                   
-                  KML(get0(variav_name), file = paste0(variavs[j], ".kml"), overwrite = T, #[length(tS_f)]
+                  KML(get0(variav_name), file = paste0(variavs[j], "_", local, ".kml"), overwrite = T, #[length(tS_f)]
                       col = color(20), blur = 1) 
                   
             }
@@ -511,8 +642,6 @@ for (j in 1:length(variavs)) {
 }
 #raster kml
 #KML(raster_IGPH, file='meuse_b2-0.kml', time =(seq(as.Date("2010-02-01"), length=2, by="1 month") -1), col = rgb.palette.rad(400), blur = 2)
-
-save.image(file = "output/data.RData")
 
 if (nc_out == 1) {
       message("creating netcdf files")
