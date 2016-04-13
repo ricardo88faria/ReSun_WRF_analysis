a17e46c7d2ccb1f0f649856441180965dad4c5e9
Ricardo Faria <r1c4rd0f4@gmail.com>
2016-04-13 14:31:31 +0100

    big update

--- Shiny_app/app.R
+++ Shiny_app/app.R
@@ -13,14 +13,15 @@ library(httr)
 #library(sp)
 library(ggmap)
 
-options(shiny.error=browser)
+#options(shiny.error=browser)
 
 load("data/data.RData") # output/
 load("data/data_coords.RData")
 load("data/data_merge.RData")
 #source("../matrix_rotation.R")
 lati <- lat
-
+lat_res <- abs(round((lati[1] - lati[2])/2, digits = 5))
+lon_res <- abs(round((long[1] - long[2])/2, digits = 5))
 #decades <- seq_time
 seq_time <- seq(00, 12, by =1)
 lon <- -16.7
@@ -36,6 +37,8 @@ d <- rbind(d, subset(d_tot, Month=="TMY_corr"))
 rasterOptions(timer = T, progress = "text")
 x <- raster_IGPH
 #x <- disaggregate(raster_IGPH, fact=c(2, 2), method='bilinear')
+min_vals <- round(min(raster_IGPH@data@min), digits = -1)
+max_vals <- round(max(raster_IGPH@data@max), digits = -1)
 
 # porto santo
 ps <- raster_IGPH_merg
@@ -45,16 +48,26 @@ ps <- raster_IGPH_merg
 
 
 # matrix to data.frame do IGPH, hgt
-# IGPH
+# IGPH mad & ps
 IGPH_melt <- IGPH[,,1]
 dimnames(IGPH_melt) = list(lati, long)
 IGPH_melt <- melt(IGPH_melt)
 colnames(IGPH_melt) <- c("lat", "lon", "val")
+# porto santo
+IGPH_merg_melt <- IGPH_merg[,,1]
+dimnames(IGPH_merg_melt) = list(lat_ps, long_ps)
+IGPH_merg_melt <- melt(IGPH_merg_melt)
+colnames(IGPH_merg_melt) <- c("lat", "lon", "val")
+IGPH_melt <- rbind(IGPH_merg_melt, IGPH_melt)
 # HGT
 dimnames(hgt) = list(lati, long)
 hgt_melt <- melt(hgt)
 colnames(hgt_melt) <- c("lat", "lon", "val")
-
+# porto santo
+dimnames(hgt_ps) = list(lat_ps, long_ps)
+hgt_ps_melt <- melt(hgt_ps)
+colnames(hgt_ps_melt) <- c("lat", "lon", "val")
+hgt_melt <- rbind(hgt_ps_melt, hgt_melt)
 
 # knitr ui
 ui <- bootstrapPage( #theme = shinytheme("journal"),
@@ -63,9 +76,10 @@ ui <- bootstrapPage( #theme = shinytheme("journal"),
       absolutePanel(top=10, right=10, draggable = F,
                     sliderInput("date", "Mês", min=min(seq_time), max=max(seq_time), value=seq_time[1], step=1, sep="", 
                                 animate = animationOptions(interval = 2500, loop = F)), # , post=" Mês"  , playButton = "PLAY", pauseButton = "PAUSA"
-                    checkboxInput("EMAS", "Mostrar EMAS e pontos", TRUE),
-                    checkboxInput("legend", "Mostrar legenda", TRUE), # NEW LINE
-                    sliderInput("opac", "Opacitade do Raster", min=0, max=1, value=.6, step=.1, sep=""), # para opacidade
+                    checkboxInput("legend", "Legenda adaptada para valores anuais (desabilitar para maior gama valores)", TRUE),
+                    checkboxInput("units", "Mudar unidades para [Wh/m^2.dia]", FALSE), 
+                    sliderInput("opac", "Opacitade do Raster", min=0, max=1, value=.8, step=.1, sep=""), # para opacidade
+                    checkboxInput("EMAS", "Mostrar EMAS e pontos do Utilizador", TRUE),
                     conditionalPanel("input.EMAS == true",
                                      selectInput("location", "Localização das EMAS", c("", levels(locs$loc)), selected=""),
                                      conditionalPanel("input.location !== null && input.location !== ''",
@@ -82,25 +96,39 @@ ui <- bootstrapPage( #theme = shinytheme("journal"),
                     style = "opacity: 0.9"
       ),
       
-      absolutePanel(bottom = 5, left = 5, width = 700, draggable = TRUE,
-                    wellPanel(
-                          HTML(markdownToHTML(fragment.only=TRUE,title = T, text=c(
-                                "Notas:
-                                1- O intante Mês 0, corresponde à média anual do TMY ajustado com as medições.
-                                2- Os valores mensais são representados sem correção, uma vez que a correção foi aplicada à média anual.
-                                3- Os meses que compõem o Ano Meteorológico Tipico a simular com o ReSun tiveram por base 10 anos medidos na sua estimativa.
-                                4- EMAS (Estações Meteorológicas Automáticas).
-                                5- Clique num ponto no mapa para ver mais detalhes sobre esse ponto."
-                          ))), actionButton("about", "Sobre", class="btn-block")
-                          ),
+      absolutePanel(bottom = 0, right = 10, width = 700, height = "auto", draggable = F, fixed = TRUE,
+                    bsCollapse(id = "collapseExample", open = NULL,
+                               bsCollapsePanel("Notas:", 
+                                               "1- O intante Mês 0, corresponde à média anual do TMY ajustado com as medições.
+                                               2- Os valores mensais são representados sem correção, uma vez que a correção foi aplicada à média anual.
+                                               3- Os meses que compõem o Ano Meteorológico Tipico a simular com o ReSun tiveram por base 10 anos medidos na sua estimativa.
+                                               4- EMAS (Estações Meteorológicas Automáticas).
+                                               5- Clique num ponto no mapa para ver mais detalhes sobre esse ponto.", 
+                                               style = "info", 
+                                               actionButton("about", "Sobre", class="btn-block"))
+                    ), 
                     style = "opacity: 0.9"
-                          ),
+      ),
+      
+      #absolutePanel(bottom = 0, left = 10, width = 700, height = "auto", draggable = TRUE, fixed = TRUE,
+      #              wellPanel(
+      #                    HTML(markdownToHTML(fragment.only=TRUE,title = T, text=c(
+      #                          "Notas:
+      #                          1- O intante Mês 0, corresponde à média anual do TMY ajustado com as medições.
+      #                          2- Os valores mensais são representados sem correção, uma vez que a correção foi aplicada à média anual.
+      #                          3- Os meses que compõem o Ano Meteorológico Tipico a simular com o ReSun tiveram por base 10 anos medidos na sua estimativa.
+      #                          4- EMAS (Estações Meteorológicas Automáticas).
+      #                          5- Clique num ponto no mapa para ver mais detalhes sobre esse ponto."
+      #                    ))), actionButton("about", "Sobre", class="btn-block")
+      #                    ),
+      #              style = "opacity: 0.9"
+      #                    ),
       
       bsModal("abouttext", "Sobre o Atlas Solar", "about", 
               HTML(markdownToHTML(fragment.only=TRUE, text=c(
-                    "Estre trabalho é realizado em parceria pelo LREC e MJInovação, com a utilização do software WRF para modelação climática e o ReSun (Pereira R., 2013), para modelação Solar.
+                    "Este trabalho foi realizado no âmbito do estágio professional de Ricardo Jorge Agrela Faria (LREC), através do código de modelação solar ReSun (PereiraR., 2013).
                     
-                    Autor do trabalho: Ricardo Faria"
+                    Autor: Ricardo Jorge Agrela Faria"
               )))
               ),
       
@@ -115,16 +143,48 @@ server <- function(input, output, session) { # added ps for another raster, port
       acm_defaults <- function(map, x, y) addCircleMarkers(map, x, y, radius=6, color="black", fillColor="orange", fillOpacity=1, opacity=1, weight=2, stroke=TRUE, layerId="Selected")
       
       # rasters
-      ras <- reactive({ subset(x, which(seq_time==input$date)) })
+      ras <- reactive({ 
+            if (input$units) {
+                  24*subset(x, which(seq_time==input$date))
+            } else { 
+                  subset(x, which(seq_time==input$date))
+            }
+      })
       #hgt_polylines <- reactive({ hgt_melt })
       #ras_ploy <- reactive({ hgt_melt })
-      ras_ps <- reactive({ subset(ps, which(seq_time==input$date)) })
+      ras_ps <- reactive({ 
+            if (input$units) {
+                  24*subset(ps, which(seq_time==input$date)) 
+            } else { 
+                  subset(ps, which(seq_time==input$date))
+            }
+      })
       
       # storage of mouse clicks circles locations lat long
       #dat <- reactiveValues(circs = data.frame(longitude=numeric(), latitude=numeric()))
       
       # color pallete bins
-      ras_vals <- reactive({ c(seq(round(min(raster_IGPH$IGPH_tot[], na.rm = T), digits = -1), round(max(raster_IGPH$IGPH_tot[], na.rm = T), digits = -1), 10)) }) #c(0, maxIGPH)
+      ras_vals <- reactive({ 
+            if (input$units) {
+                  c(seq(24*min_vals, 24*max_vals, 400)) 
+            } else { 
+                  c(seq(min_vals, max_vals, 15)) 
+            }  
+      }) #c(0, maxIGPH)
+      ras_vals_anual <- reactive({ 
+            if (input$units) {
+                  c(seq(24*round(min(raster_IGPH$IGPH_tot[], na.rm = T), digits = -2), 24*round(max(raster_IGPH$IGPH_tot[], na.rm = T), digits = -1), 200)) 
+            } else { 
+                  c(seq(round(min(raster_IGPH$IGPH_tot[], na.rm = T), digits = -1), round(max(raster_IGPH$IGPH_tot[], na.rm = T), digits = -1), 10)) 
+            } 
+      })
+      legend_tytle <- reactive({ 
+            if (input$units) {
+                  paste("Radiação Global [Wh/m^2.dia]")
+            } else { 
+                  paste("Radiação Global [W/m^2]")
+            } 
+      })
       
       # color paletes
       #colorNumeric(palette = c("dodgerblue", "springgreen2", "yellow2", "orange", "tomato1", "violetred4", "purple"), domain = ras_vals(), na.color="transparent")
@@ -132,6 +192,8 @@ server <- function(input, output, session) { # added ps for another raster, port
       #colorBin(c('#fee0d2', '#fcbba1', '#fc9272', '#fb6a4a', '#ef3b2c', '#cb181d', '#a50f15', '#67000d'), bins = c(0, 5, 8, 10, 12, 14, 18, 24, 26))
       pal <- reactive({ colorBin(palette = c("dodgerblue", "springgreen2", "yellow2", "orange", "tomato1", "violetred4", "purple"), domain = ras_vals(), bins = c(-Inf, ras_vals(), Inf), na.color="transparent", alpha = F) }) 
       pal_legend <- reactive({ colorBin(palette = c("dodgerblue", "springgreen2", "yellow2", "orange", "tomato1", "violetred4", "purple"), domain = ras_vals(), bins = ras_vals(), na.color="transparent", alpha = F) }) 
+      pal_anual <- reactive({ colorBin(palette = c("dodgerblue", "springgreen2", "yellow2", "orange", "tomato1", "violetred4", "purple"), domain = ras_vals_anual(), bins = c(-Inf, ras_vals_anual(), Inf), na.color="transparent", alpha = F) }) 
+      pal_legend_anual <- reactive({ colorBin(palette = c("dodgerblue", "springgreen2", "yellow2", "orange", "tomato1", "violetred4", "purple"), domain = ras_vals_anual(), bins = ras_vals_anual(), na.color="transparent", alpha = F) }) 
       
       output$Map <- renderLeaflet({ 
             leaflet() %>% 
@@ -140,23 +202,21 @@ server <- function(input, output, session) { # added ps for another raster, port
                   #addWMSTiles("http://www.lrec.pt/", attribution = "Mapa Rad. Solar © 2015 - Ricardo Faria, LREC, MJInovação") %>%
                   #addPolygons(ras_ploy(), lng = long, lat = lati, opacity=0.9, popup = popup_test) %>%
                   #addPolylines(hgt_polylines(), lng = long, lat = lati, color = "red") %>% 
-                  addProviderTiles("OpenTopoMap") %>% # modify thebackground map "Esri.WorldImagery" "OpenTopoMap"
+                  #addProviderTiles("OpenTopoMap") %>% # modify thebackground map "Esri.WorldImagery" "OpenTopoMap"
                   addCircleMarkers(data=locs, radius=6, color="black", stroke=FALSE, fillOpacity=0.5, group="locations", layerId = ~loc)
       })
       
       observe({
             proxy <- leafletProxy("Map")
-            # proxy %>% addRasterImage(ras(), colors=pal(), opacity=input$opac) %>% addRasterImage(ras_ps(), colors=pal(), opacity=input$opac)
-            proxy %>% removeTiles(layerId="raster") %>% #addPopups(ras()) %>% 
-                  addRasterImage(ras(), colors=pal(), opacity=input$opac, layerId="raster") %>% addRasterImage(ras_ps(), colors=pal(), opacity=input$opac, layerId="porto_santo")
-      })
-      
-      
-      observe({
-            proxy <- leafletProxy("Map")
             proxy %>% clearControls()
             if (input$legend) {
-                  proxy %>% addLegend(position="bottomright", pal=pal_legend(), values=ras_vals(), title="Radiação Global [W/m^2]") # values= seq(50, 220, 5)
+                  proxy %>% addLegend(position="bottomleft", pal=pal_legend_anual(), values=ras_vals_anual(), title=legend_tytle()) %>% # values= seq(50, 220, 5)
+                        addRasterImage(ras(), colors=pal_anual(), opacity=input$opac, layerId="raster") %>% 
+                        addRasterImage(ras_ps(), colors=pal_anual(), opacity=input$opac, layerId="porto_santo")
+            } else {
+                  proxy %>% addLegend(position="bottomleft", pal=pal_legend(), values=ras_vals(), title=legend_tytle()) %>% # values= seq(50, 220, 5)
+                        addRasterImage(ras(), colors=pal(), opacity=input$opac, layerId="raster") %>% 
+                        addRasterImage(ras_ps(), colors=pal(), opacity=input$opac, layerId="porto_santo")
             }
       })
       
@@ -177,14 +237,26 @@ server <- function(input, output, session) { # added ps for another raster, port
             click <- input$Map_click
             clat <- round(click$lat, digits = 4)
             clng <- round(click$lng, digits = 4)
-            rad_val <- subset(IGPH_melt, lat<(clat+0.00045) & lat>(clat-0.00045) & lon<(clng+0.00045) & lon>(clng-0.00045))
+            
+            rad_val <- subset(IGPH_melt, lat<(clat+lat_res) & lat>(clat-lat_res) & lon<(clng+lon_res) & lon>(clng-lon_res))
+            if (input$units) {
+                  rad_val <- 24*rad_val
+            } else { 
+                  rad_val <- rad_val
+            } 
             rad_val <- round(rad_val, digits = 3)
-            hgt_val <- subset(hgt_melt, lat<(clat+0.00045) & lat>(clat-0.00045) & lon<(clng+0.00045) & lon>(clng-0.00045)) # 0.00045 = resolução da matriz = long[2] - long[1]
+            hgt_val <- subset(hgt_melt, lat<(clat+lat_res) & lat>(clat-lat_res) & lon<(clng+lon_res) & lon>(clng-lon_res)) # 0.00045 = resolução da matriz = long[2] - long[1]
             hgt_val <- round(hgt_val, digits = 3)
+            
+            if (input$units) {
+                  rad_units <- paste(" [Wh/m^2.dia]")
+            } else { 
+                  rad_units <- paste(" [W/m2]")
+            }
             # html colors #7f0000, #0375DB
             popup_click <- paste0("<span style='color: #0375DB'><strong>Valor da Radiação anual com a correção TMY no ponto selecionado.</strong></span>", 
                                   "<br><span style='color: salmon;'><strong>Radiação Global anual: </strong></span>",
-                                  rad_val, " [W/m2]",
+                                  rad_val, rad_units,
                                   "<br><span style='color: salmon;'><strong>Altura do nível do mar: </strong></span>",
                                   hgt_val, " [m]",
                                   "<br><span style='color: salmon;'><strong>Coordenadas (Long, Lat): </strong></span>",
@@ -193,7 +265,7 @@ server <- function(input, output, session) { # added ps for another raster, port
             # option to hideGroup('circles')
             proxy <- leafletProxy("Map") # use the proxy to save computation
             proxy %>% addCircles(lng=clng, lat=clat, group='locations',
-                                 weight=5, radius=100, color='black', fillColor='blue',
+                                 weight=5, radius=100, color='#0375DB', fillColor='#0375DB',
                                  popup=popup_click, fillOpacity=1, opacity=1, stroke=T, layerId="Selected") # se retirar o (layerId="Selected") passa a aceitar varios pontos 
       })
       
@@ -231,14 +303,28 @@ server <- function(input, output, session) { # added ps for another raster, port
       })
       
       # knitr server pointdata
-      Data <- reactive({ d %>% filter(Location==input$location) })
+      Data <- reactive({ 
+            if (input$units) {
+                  d$value <- d$value*24
+                  d %>% filter(Location==input$location)
+            } else { 
+                  d %>% filter(Location==input$location)
+            } 
+            
+      })
       Data_tot <- reactive({ d })
       
-      output$TestPlot <- renderPlot({ ggplot(Data(), aes(x = Month ,y = value, color = Location, group = Location)) + 
+      output$TestPlot <- renderPlot({ 
+            if (input$units) {
+                  rad_units <- paste("[Wh/m^2.dia]")
+            } else { 
+                  rad_units <- paste("[W/m2]")
+            }
+            ggplot(Data(), aes(x = Month ,y = value, color = Location, group = Location)) + 
                   geom_bar(stat = "identity", position = "dodge") + 
                   scale_fill_brewer(palette="Paired") + 
                   theme_bw() +
-                  ggtitle("Radiação mensal sem TMY_corr nas EMAS [W/m^2]") })
+                  ggtitle(paste("Radiação mensal sem TMY_corr nas EMAS ", rad_units)) })
       
       #output$TestPlot1 <- renderPlot({ ggplot(Data_tot(), aes(x = Month ,y = value, color = Location, group = Location)) + 
       #            geom_bar(stat = "identity", position = "dodge") + 
