f55929135b0d9edb91eac15bde5528459c20b424
Ricardo Faria <r1c4rd0f4@gmail.com>
2016-04-14 11:51:38 +0100

    add monthly values on map click

--- Shiny_app/app.R
+++ Shiny_app/app.R
@@ -18,7 +18,7 @@ library(ggmap)
 load("data/data.RData") # output/
 load("data/data_coords.RData")
 load("data/data_merge.RData")
-#source("../matrix_rotation.R")
+source("../matrix_rotation.R")
 
 lat_res <- abs(round((lat[1] - lat[2])/2, digits = 5))
 lon_res <- abs(round((long[1] - long[2])/2, digits = 5))
@@ -48,6 +48,7 @@ ps <- raster_IGPH_merg
 #maxIGPH <- round(max(as.numeric(max_IGPH)+5),-1)
 
 # matrix to data.frame do IGPH, hgt
+# x <- subset(x, which(seq_time==00))
 # IGPH mad & ps
 IGPH_melt <- IGPH[,,1]
 dimnames(IGPH_melt) = list(lat, long)
@@ -74,7 +75,7 @@ ui <- bootstrapPage( #theme = shinytheme("journal"),
       tags$style( "html, body {width:100%;height:100%}"), #type="text/css",
       leafletOutput("Map", width="100%", height="100%"),
       absolutePanel(top=10, right=10, draggable = F,
-                    sliderInput("date", "Mês", min=min(seq_time), max=max(seq_time), value=seq_time[1], step=1, sep="", 
+                    sliderInput("date", "Mês", min=min(seq_time), max=max(seq_time), value=seq_time[1], step=1, sep="", #format="## Months", timeFormat = "%B",
                                 animate = animationOptions(interval = 2500, loop = F)), # , post=" Mês"  , playButton = "PLAY", pauseButton = "PAUSA"
                     checkboxInput("legend", "Legenda adaptada para valores anuais (desabilitar para maior gama valores)", TRUE),
                     checkboxInput("units", "Mudar unidades para [Wh/m^2.dia]", FALSE), 
@@ -141,6 +142,11 @@ ui <- bootstrapPage( #theme = shinytheme("journal"),
 # knitr server
 server <- function(input, output, session) { # added ps for another raster, porto santo
       acm_defaults <- function(map, x, y) addCircleMarkers(map, x, y, radius=6, color="black", fillColor="orange", fillOpacity=1, opacity=1, weight=2, stroke=TRUE, layerId="Selected")
+      
+      # month
+      month_input <- reactive({ 
+            months_name[input$date+1]
+      })
       
       # rasters
       ras <- reactive({ 
@@ -249,6 +255,7 @@ server <- function(input, output, session) { # added ps for another raster, port
             clat <- round(click$lat, digits = 4)
             clng <- round(click$lng, digits = 4)
             
+            # global rad 
             rad_val <- subset(IGPH_melt, lat<(clat+lat_res) & lat>(clat-lat_res) & lon<(clng+lon_res) & lon>(clng-lon_res))
             if (input$units) {
                   rad_val <- 24*rad_val
@@ -259,6 +266,21 @@ server <- function(input, output, session) { # added ps for another raster, port
             hgt_val <- subset(hgt_melt, lat<(clat+lat_res) & lat>(clat-lat_res) & lon<(clng+lon_res) & lon>(clng-lon_res)) # 0.00045 = resolução da matriz = long[2] - long[1]
             hgt_val <- round(hgt_val, digits = 3)
             
+            # monthly rad 
+            x_month_rad <- mat_rot(mat_rot(mat_rot(t(as.matrix(as.data.frame(as.matrix(ras())))))))
+            dimnames(x_month_rad) = list(lat, long)
+            x_month_rad <- melt(x_month_rad)
+            colnames(x_month_rad) <- c("lat", "lon", "val")
+            #porto santo:
+            ps_month_rad <- mat_rot(mat_rot(mat_rot(t(as.matrix(as.data.frame(as.matrix(ras_ps())))))))
+            dimnames(ps_month_rad) = list(lat_ps, long_ps)
+            ps_month_rad <- melt(ps_month_rad)
+            colnames(ps_month_rad) <- c("lat", "lon", "val")
+            
+            month_rad <- rbind(ps_month_rad, x_month_rad)
+            month_rad <- subset(month_rad, lat<(clat+lat_res) & lat>(clat-lat_res) & lon<(clng+lon_res) & lon>(clng-lon_res))
+            month_rad <- round(month_rad, digits = 3)
+            
             if (input$units) {
                   rad_units <- paste(" [Wh/m^2.dia]")
             } else { 
@@ -268,6 +290,8 @@ server <- function(input, output, session) { # added ps for another raster, port
             popup_click <- paste0("<span style='color: #0375DB'><strong>Valor da Radiação anual com a correção TMY no ponto selecionado.</strong></span>", 
                                   "<br><span style='color: salmon;'><strong>Radiação Global anual: </strong></span>",
                                   rad_val, rad_units,
+                                  "<br><span style='color: salmon;'><strong>Radiação Mensal s/ correção TMY: </strong></span>",
+                                  month_rad, rad_units, paste0(" (",month_input(), ")"),
                                   "<br><span style='color: salmon;'><strong>Altura do nível do mar: </strong></span>",
                                   hgt_val, " [m]",
                                   "<br><span style='color: salmon;'><strong>Coordenadas (Long, Lat): </strong></span>",
