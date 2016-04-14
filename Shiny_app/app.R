1c99adf23233d1e9a53cede078253a1d57385057
Ricardo Faria <r1c4rd0f4@gmail.com>
2016-04-14 19:25:47 +0100

    add mat_totation function and monthly stats to user points

--- Shiny_app/app.R
+++ Shiny_app/app.R
@@ -18,7 +18,7 @@ library(ggmap)
 load("data/data.RData") # output/
 load("data/data_coords.RData")
 load("data/data_merge.RData")
-source("../matrix_rotation.R")
+source("matrix_rotation.R")
 
 lat_res <- abs(round((lat[1] - lat[2])/2, digits = 5))
 lon_res <- abs(round((long[1] - long[2])/2, digits = 5))
@@ -70,6 +70,14 @@ hgt_ps_melt <- melt(hgt_ps)
 colnames(hgt_ps_melt) <- c("lat", "lon", "val")
 hgt_melt <- rbind(hgt_ps_melt, hgt_melt)
 
+# extract values from raster to data frame to use in popup monthly
+vals<-extract(x ,1:ncell(x))
+coord<-xyFromCell(x ,1:ncell(x))
+combine<-cbind(coord,vals)
+colnames(combine) <- c("lon", "lat", months_name)
+combine <- data.frame(combine)
+
+
 # knitr ui
 ui <- bootstrapPage( #theme = shinytheme("journal"),
       tags$style( "html, body {width:100%;height:100%}"), #type="text/css",
@@ -79,6 +87,11 @@ ui <- bootstrapPage( #theme = shinytheme("journal"),
                                 animate = animationOptions(interval = 2500, loop = F)), # , post=" Mês"  , playButton = "PLAY", pauseButton = "PAUSA"
                     checkboxInput("legend", "Legenda adaptada para valores anuais (desabilitar para maior gama valores)", TRUE),
                     checkboxInput("units", "Mudar unidades para [Wh/m^2.dia]", FALSE), 
+                    checkboxInput("addMarker", "Adicionar marcador ao clicar no mapa."),
+                    #textInput("userlat", "Latitude do ponto em graus decimais:", "32.648904"),
+                    #textInput("userlong", "longitude  do ponto em graus decimais:", "-16.940765"),
+                    #actionButton("coordsinsert", "inserir coordenadas para criar marcador"),
+                    actionButton("clearMarkers", "Limpar marcadores"),
                     sliderInput("opac", "Opacitade do Raster", min=0, max=1, value=.8, step=.1, sep=""), # para opacidade
                     checkboxInput("EMAS", "Mostrar EMAS e pontos do Utilizador", TRUE),
                     conditionalPanel("input.EMAS == true",
@@ -143,6 +156,13 @@ ui <- bootstrapPage( #theme = shinytheme("journal"),
 server <- function(input, output, session) { # added ps for another raster, porto santo
       acm_defaults <- function(map, x, y) addCircleMarkers(map, x, y, radius=6, color="black", fillColor="orange", fillOpacity=1, opacity=1, weight=2, stroke=TRUE, layerId="Selected")
       
+      # latlon
+      lat_input <- reactive({ 
+            input$userlat
+      })
+      lon_input <- reactive({ 
+            input$userlon
+      })
       # month
       month_input <- reactive({ 
             months_name[input$date+1]
@@ -228,12 +248,12 @@ server <- function(input, output, session) { # added ps for another raster, port
             proxy %>% clearControls()
             if (input$legend) {
                   proxy %>% addLegend(position="bottomleft", pal=pal_legend_anual(), values=minmax_vals_anual_leg(), title=legend_tytle()) %>% # values= seq(50, 220, 5)
-                        addRasterImage(ras(), colors=pal_anual(), opacity=input$opac, layerId="raster") %>% 
-                        addRasterImage(ras_ps(), colors=pal_anual(), opacity=input$opac, layerId="porto_santo")
+                        addRasterImage(ras(), colors=pal_anual(), opacity=input$opac, project = F, layerId="raster") %>% 
+                        addRasterImage(ras_ps(), colors=pal_anual(), opacity=input$opac, project = F, layerId="porto_santo")
             } else {
                   proxy %>% addLegend(position="bottomleft", pal=pal_legend(), values=minmax_vals_leg(), title=legend_tytle()) %>% # values= seq(50, 220, 5)
-                        addRasterImage(ras(), colors=pal(), opacity=input$opac, layerId="raster") %>% 
-                        addRasterImage(ras_ps(), colors=pal(), opacity=input$opac, layerId="porto_santo")
+                        addRasterImage(ras(), colors=pal(), opacity=input$opac, project = F,  layerId="raster") %>% 
+                        addRasterImage(ras_ps(), colors=pal(), opacity=input$opac, project = F, layerId="porto_santo")
             }
       })
       
@@ -252,8 +272,14 @@ server <- function(input, output, session) { # added ps for another raster, port
       observeEvent(input$Map_click, {
             # Get the click info
             click <- input$Map_click
-            clat <- round(click$lat, digits = 4)
-            clng <- round(click$lng, digits = 4)
+            
+            #if(input$coordsinsert){
+            #      clat <- round(lat_input(), digits = 4)
+            #      clng <- round(lon_input(), digits = 4)
+            #} else {
+                  clat <- round(click$lat, digits = 4)
+                  clng <- round(click$lng, digits = 4)
+            #}
             
             # global rad 
             rad_val <- subset(IGPH_melt, lat<(clat+lat_res) & lat>(clat-lat_res) & lon<(clng+lon_res) & lon>(clng-lon_res))
@@ -267,18 +293,23 @@ server <- function(input, output, session) { # added ps for another raster, port
             hgt_val <- round(hgt_val, digits = 3)
             
             # monthly rad 
-            x_month_rad <- mat_rot(mat_rot(mat_rot(t(as.matrix(as.data.frame(as.matrix(ras())))))))
-            dimnames(x_month_rad) = list(lat, long)
-            x_month_rad <- melt(x_month_rad)
-            colnames(x_month_rad) <- c("lat", "lon", "val")
+            #x_month_rad <- mat_rot(mat_rot(mat_rot(t(as.matrix(as.data.frame(as.matrix(ras())))))))
+            #dimnames(x_month_rad) = list(lat, long)
+            #x_month_rad <- melt(x_month_rad)
+            #colnames(x_month_rad) <- c("lat", "lon", "val")
             #porto santo:
-            ps_month_rad <- mat_rot(mat_rot(mat_rot(t(as.matrix(as.data.frame(as.matrix(ras_ps())))))))
-            dimnames(ps_month_rad) = list(lat_ps, long_ps)
-            ps_month_rad <- melt(ps_month_rad)
-            colnames(ps_month_rad) <- c("lat", "lon", "val")
+            #ps_month_rad <- mat_rot(mat_rot(mat_rot(t(as.matrix(as.data.frame(as.matrix(ras_ps())))))))
+            #dimnames(ps_month_rad) = list(lat_ps, long_ps)
+            #ps_month_rad <- melt(ps_month_rad)
+            #colnames(ps_month_rad) <- c("lat", "lon", "val")
+            #rbind tables
+            #month_rad <- rbind(ps_month_rad, x_month_rad)
+            #month_rad <- subset(month_rad, lat<(clat+lat_res) & lat>(clat-lat_res) & lon<(clng+lon_res) & lon>(clng-lon_res))
+            #month_rad <- round(month_rad, digits = 3)
             
-            month_rad <- rbind(ps_month_rad, x_month_rad)
-            month_rad <- subset(month_rad, lat<(clat+lat_res) & lat>(clat-lat_res) & lon<(clng+lon_res) & lon>(clng-lon_res))
+            month_rad <- subset(combine, lat<(clat+lat_res) & lat>(clat-lat_res) & lon<(clng+lon_res) & lon>(clng-lon_res))
+            month_rad <- month_rad[,4:length(month_rad)]
+            #month_rad <- month_rad[,c(month_input())]
             month_rad <- round(month_rad, digits = 3)
             
             if (input$units) {
@@ -290,18 +321,27 @@ server <- function(input, output, session) { # added ps for another raster, port
             popup_click <- paste0("<span style='color: #0375DB'><strong>Valor da Radiação anual com a correção TMY no ponto selecionado.</strong></span>", 
                                   "<br><span style='color: salmon;'><strong>Radiação Global anual: </strong></span>",
                                   rad_val, rad_units,
+                                  
                                   "<br><span style='color: salmon;'><strong>Radiação Mensal s/ correção TMY: </strong></span>",
-                                  month_rad, rad_units, paste0(" (",month_input(), ")"),
+                                  list(month_rad), rad_units,# paste0(" (",month_input(), ")"),
+                                  
                                   "<br><span style='color: salmon;'><strong>Altura do nível do mar: </strong></span>",
                                   hgt_val, " [m]",
+                                  
                                   "<br><span style='color: salmon;'><strong>Coordenadas (Long, Lat): </strong></span>",
                                   clng, ", ",clat, " [º]")
             # Add the circle to the map proxy
             # option to hideGroup('circles')
-            proxy <- leafletProxy("Map") # use the proxy to save computation
-            proxy %>% addCircles(lng=clng, lat=clat, group='locations',
-                                 weight=5, radius=100, color='#0375DB', fillColor='#0375DB',
-                                 popup=popup_click, fillOpacity=1, opacity=1, stroke=T, layerId="Selected") # se retirar o (layerId="Selected") passa a aceitar varios pontos 
+            if (input$addMarker) {
+                  proxy <- leafletProxy("Map") # use the proxy to save computation
+                  proxy %>% addCircles(lng=clng, lat=clat, group='locations',
+                                       weight=5, radius=100, color='#0375DB', fillColor='#0375DB',
+                                       popup=popup_click, fillOpacity=1, opacity=1, stroke=T, layerId=NULL) # se retirar o (layerId="Selected") passa a aceitar varios pontos 
+            }
+      })
+      
+      observeEvent(input$clearMarkers, {
+            leafletProxy("Map") %>% clearShapes() 
       })
       
       # update the map markers and view on map clicks
